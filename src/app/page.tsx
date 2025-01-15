cf80c3a3cbce3a05ffd4a5967d7312aa33ac34e2
Frompaje <yanedwards11@gmail.com>
2025-01-15 14:09:51 -0300

    feat:  homepage with new content and button for account creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ import { Header } from '@/pages/header'
 
 export default function Home() {
   return (
-    <main className="bg-customGray h-screen  text-white font-semibold">
+    <main className="bg-customGray h-screen  text-customGreen font-semibold flex flex-col p-2">
       <Header />
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-extrabold max-w-96 text-center">
+          Controle suas inscrições e acompanhe seu progresso de forma eficiente
+        </h1>
+      </div>
+      <p className="text-wrap text-xl ">
+        O <span className="font-bold">Workflow</span> foi desenvolvido para
+        candidatos que desejam ter um controle completo sobre suas inscrições em
+        vagas de emprego. Com ele, você pode acompanhar de forma clara e precisa
+        o status de todas as suas candidaturas.
+      </p>
+      <button className="bg-white border-1 rounded-lg cursor-pointer p-1 hover:bg-customGreen hover:text-white transition ease-in-out duration-300 p-2">
+        Criar conta
+      </button>
     </main>
   )
 }
